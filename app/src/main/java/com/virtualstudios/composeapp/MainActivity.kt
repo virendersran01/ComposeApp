4d33504c921942e817ae10603c2259ea96b10e59
virender sran <virendersran01@gmail.com>
2021-08-27 17:33:06 +0530

    On Section 5 Practics Jetpack Compose Layouts codelabs. https://developer.android.com/codelabs/jetpack-compose-layouts?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fcompose%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-layouts#4

--- app/src/main/java/com/virtualstudios/composeapp/MainActivity.kt
+++ app/src/main/java/com/virtualstudios/composeapp/MainActivity.kt
@@ -16,18 +16,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.virtualstudios.composeapp.ui.theme.ComposeAppTheme
@@ -46,7 +43,8 @@ class MainActivity : ComponentActivity() {
             MyApp {
                 //MyScreenContentList()
                 //MyScreenContent()
-                MyScreenContentWithScroll()
+               // MyScreenContentWithScroll()
+               HomeScreen()
             }
         }
     }
@@ -65,7 +63,54 @@ fun MyApp(content: @Composable () -> Unit){
 @Composable
 fun PreviewMyApp(){
     MyApp {
-        MyScreenContentWithScroll()
+       HomeScreen()
+    }
+
+}
+
+@Composable
+fun HomeScreen(){
+    Scaffold { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier){
+    Column(modifier) {
+        Text("Hello")
+        Text("Android Developer\n is Compose awesome?")
+    }
+}
+
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier){
+    Row(modifier = modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.onSurface)
+        .clickable { }
+        .padding(16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+
+        Column(modifier = Modifier
+            .padding(start = 8.dp)
+            .align(alignment = Alignment.CenterVertically)
+        ) {
+            Text(text = "Andy Rubin", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+
+        }
+
     }
 }
 
@@ -80,7 +125,7 @@ fun DisplayTextWithClick(text: String){
     modifier = Modifier
         .padding(24.dp)
         .background(color = backgroundColor)
-        .clickable(onClick = { isSelected = !isSelected})
+        .clickable(onClick = { isSelected = !isSelected })
     )
 }
 
